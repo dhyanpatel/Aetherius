d844e93b7d1b15741d1a403387f99f59a988a893
dpp2000 <dhyanpp@gmail.com>
2017-06-29 02:06:47 -0500

    deletes messages!

--- recruitment.py
+++ recruitment.py
@@ -16,72 +16,91 @@ class Recruitment:
         embedded = discord.Embed(color = discord.Color.dark_grey())
         embedded.add_field(name = 'IGN',
                            value='What\'s your IGN?')
-        await self.bot.say(embed = embedded)
+        one = await self.bot.say(embed = embedded)
         ign = await self.bot.wait_for_message(author=ctx.message.author)
+        await self.bot.delete_message(one)
+        await self.bot.delete_message(ign)
 
         #Rank
         await self.bot.send_typing(ctx.message.channel)
         embedded = discord.Embed(color=discord.Color.dark_grey())
         embedded.add_field(name = 'Rank',
                            value = 'What Rank were you in Spring Season 2017?\n(You must be higher than Tier 7 and provide a picture of proof')
-        await self.bot.say(embed = embedded)
+        one = await self.bot.say(embed = embedded)
         rank = await self.bot.wait_for_message(author = ctx.message.author)
+        await self.bot.delete_message(one)
+        await self.bot.delete_message(rank)
 
         #Experience
         await self.bot.send_typing(ctx.message.channel)
         embedded = discord.Embed(color=discord.Color.dark_grey())
         embedded.add_field(name = 'Experience',
                            value='How long have you been playing VainGlory?')
-        await self.bot.say(embed = embedded)
+        one = await self.bot.say(embed = embedded)
         experience = await self.bot.wait_for_message(author = ctx.message.author)
+        await self.bot.delete_message(one)
+        await self.bot.delete_message(experience)
 
         #Voice Chat
         await self.bot.send_typing(ctx.message.channel)
         embedded=discord.Embed(color=discord.Color.dark_grey())
         embedded.add_field(name = 'Voice Chat',
                            value = 'How comfortable are you using Voice Chat?\nIf not, this is not the guild for you.')
-        await self.bot.say(embed = embedded)
+        one = await self.bot.say(embed = embedded)
         voice_chat = await self.bot.wait_for_message(author = ctx.message.author)
+        await self.bot.delete_message(one)
+        await self.bot.delete_message(voice_chat)
 
         #Wins
         await self.bot.send_typing(ctx.message.channel)
         embedded = discord.Embed(color = discord.Color.dark_grey())
         embedded.add_field(name = 'Wins',
                            value = 'How many wins do you have?')
-        await self.bot.say(embed = embedded)
+        one = await self.bot.say(embed = embedded)
         wins = await self.bot.wait_for_message(author = ctx.message.author)
+        await self.bot.delete_message(one)
+        await self.bot.delete_message(wins)
 
         #Discord Activity
         await self.bot.send_typing(ctx.message.channel)
         embedded = discord.Embed(color = discord.Color.dark_grey())
         embedded.add_field(name = 'Discord Activity',
                            value = 'Will you be able to be active on Discord?')
-        await self.bot.say(embed = embedded)
+        one = await self.bot.say(embed = embedded)
         discord_activity = await self.bot.wait_for_message(author=ctx.message.author)
+        await self.bot.delete_message(one)
+        await self.bot.delete_message(discord_activity)
 
         #Role
         await self.bot.send_typing(ctx.message.channel)
         embedded = discord.Embed(color = discord.Color.dark_grey())
         embedded.add_field(name = 'Role',
                            value = 'What is your preferred role?')
-        await self.bot.say(embed = embedded)
+        one = await self.bot.say(embed = embedded)
         role = await self.bot.wait_for_message(author=ctx.message.author)
+        await self.bot.delete_message(one)
+        await self.bot.delete_message(role)
 
         #Age
         await self.bot.send_typing(ctx.message.channel)
         embedded = discord.Embed(color = discord.Color.dark_grey())
         embedded.add_field(name = 'Age',
                            value = 'How old are you?\n(12 years old and higher)')
-        await self.bot.say(embed = embedded)
+        one = await self.bot.say(embed = embedded)
         age = await self.bot.wait_for_message(author=ctx.message.author)
+        await self.bot.delete_message(one)
+        await self.bot.delete_message(age)
+
 
         #Game Activity
         await self.bot.send_typing(ctx.message.channel)
         embedded = discord.Embed(color = discord.Color.dark_grey())
         embedded.add_field(name = 'Game Activity',
                            value = 'How would you rate yourself 1-10 on how active you are?')
-        await self.bot.say(embed = embedded)
+        one = await self.bot.say(embed = embedded)
         activity = await self.bot.wait_for_message(author=ctx.message.author)
+        await self.bot.delete_message(one)
+        await self.bot.delete_message(activity)
 
         await self.bot.send_typing(ctx.message.channel)
         embedded = discord.Embed(color = discord.Color.green())
